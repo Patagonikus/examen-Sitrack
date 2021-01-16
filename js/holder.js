a4b9feb1ad6c0636944f5d86db9a6e48f6c009e2
Patagonikus <corvalanemiliano@hotmail.com>
2021-01-16 11:31:46 -0300

    mejoras en la implementación de funciones de check on

--- js/holder.js
+++ js/holder.js
@@ -1,14 +1,3 @@
-// :::::: ENUM HOLDER TYPE :::::: \\
-
-// const holderType = {
-
-//     MT: "Móvil Terrestre",
-//     DEPOSITO: "Depósito",
-//     BARCO: "Barco",
-//     MAQUINA: "Máquina",
-//     ESTACIÓN: "Estación"
-
-// }
 
 // :::::: HOLDER CLASS :::::: \\
 
@@ -22,63 +11,10 @@ class Holder {
         this.nextReviewDate = nextReviewDate;
         this.holderStatus = holderStatus;
     }
-    // :::::: GETTER AND SETTER :::::: \\
 
-    getHolderType() {
-        return this.holderType;
-    }
-
-    setHolderType(holderType) {
-        this.holderType = holderType;
-    }
-
-    getChasisNumber() {
-        return this.chasisNumber;
-    }
-
-    setChasisNumber(chasisNumber) {
-        this.chasisNumber = chasisNumber;
-    }
-
-    getHolderName() {
-        return this.holderName;
-    }
-
-    setHolderName(holderName) {
-        this.holderName = holderName;
-    }
-
-    getDomainName() {
-        return this.domainName;
-    }
-
-    setDomainName(domainName) {
-        this.domainName = domainName;
-    }
-
-    getnextReviewDate() {
-        return this.nextReviewDate;
-    }
-
-    setnextReviewDate(nextReviewDate) {
-        this.nextReviewDate = nextReviewDate;
-    }
-
-    getHolderStatus() {
-        return this.holderStatus;
-    }
-
-    setHolderStatus(holderStatus) {
-        this.holderStatus = holderStatus;
-    }
 
 
 }
-
-
-// :::::: INSTANCE :::::: \\
-
-// var holder1 = new Holder("MT", 1234, "Camión 2", "ab123cd", "04-01-21", 1);
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,57 @@
 
-// window.onload = loadTableData();
+window.onload = function(){
+    loadTableData(arrHolders);
+}
+// var arrHolders = [{holder1},{holder2},{holder3}];
+
+
 var arrHolders = [];
+arrHolders.push(new Holder("Barco", 1234, "Camión 2", "ab123cd", "21", 1), new Holder("Depósito", 1234, "depo 23", "ab123cd", "21", 1));
+f = new Date();
+var day = f.getDate();
+
 
 function loadTableData(arrHolders/*[{holde},{holde},{holde},....]*/) {
     var table = document.getElementById("holders_table"),
         body = document.getElementById("holders_body"),
         bhtml = '';
-        f = new Date();
-        var day = f.getDate();
-        var button = document.createElement("button");
+
 
     body.innerHTML = '';
     for (let i = 0; i < arrHolders.length; i++) { //recorro todas las instancias de holders
         // debugger
         // alert("hola");
+        
         bhtml += `
         <tr>
-            <td>${arrHolders[i].holderType}</td>
-            <td>${arrHolders[i].chasisNumber}</td>
-            <td>${arrHolders[i].holderName}</td>
-            <td>${arrHolders[i].domainName}</td>
-            <td>${arrHolders[i].nextReviewDate}</td>
-            <td>${arrHolders[i].holderStatus   }</td>
-            <td><button></button></td>
+            <td class = "align-middle">${arrHolders[i].holderType}</td>
+            <td class = "align-middle">${arrHolders[i].chasisNumber}</td>
+            <td class = "align-middle">${arrHolders[i].holderName}</td>
+            <td class = "align-middle">${arrHolders[i].domainName}</td>
+            <td class = "align-middle">${arrHolders[i].nextReviewDate}</td>
+            <td class = "align-middle">${arrHolders[i].holderStatus}</td>
+            <td class = "align-middle">${checkOn(arrHolders[i].nextReviewDate,i,arrHolders[i].holderStatus)}</td>
         </tr>
         `;
     }
+
     body.innerHTML = bhtml; // "<tr><td>00</td><td>01</td>...</tr>...<tr><td>00</td><td>01</td>...</tr>"
+    var bButton = body.querySelectorAll("button");
+
+    for (let i = 0; i < bButton.length; i++) {
+        const element = bButton[i];
+
+        element.onclick = function (evt) {
+            debugger
+            // alert("hola boton");
+            evt.toElement.parentElement.parentElement.children[5].innerHTML = "0";
+            arrHolders[evt.toElement.id*1].holderStatus = 0;
+            evt.toElement.disabled = true;
+        }
+
+    }
 }
-{/* <td>${checkon(arrHolders[i].nextReviewDate)}</td> */}
+
 function addHolder() {
     var table = document.getElementById("holders_table");
 
@@ -49,26 +72,29 @@ function addHolder() {
 
 function randomDate(from, to) {
     return Math.round((Math.random() * (to - from) + from));
-  
-    }
 
-function checkOn(hReviewDate) { //recibe la fecha del review del holder
+}
+
+function checkOn(hReviewDate, index, status) { //recibe la fecha del review del holder
     //compara fechas actual con parámetro
-    if (day > hReviewDate) {
+    if (day > hReviewDate && status >0) {
         //buttonegar botón chbuttonkon
-        return button;
-
+        return `<button class="btn btn-warning align-middle" id="${index}">Check ON</button>`;
     }
-
+    else {
+        return "";
+    }
 }
 
-function changeStatus(status) {
-    if (status = 1) {
-        status = 0;
-    } else {
-        status = 1;
-    }
+// function changeStatus(status) {
+//     if (status = 1) {
+//         // status = 0;
+//         alert("se cambió a 0");
+//     } else {
+//         // status = 1;
+//         alert("se cambió a 1");
+//     }
 
-}
+// }
 
 
